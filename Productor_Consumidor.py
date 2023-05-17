28d15a91617052cb70e279483e63c250941fa852
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2023-05-17 11:39:53 +0200

    cambio

--- Productor_Consumidor.py
+++ Productor_Consumidor.py
@@ -2,13 +2,13 @@ import time
 import multiprocessing
 import queue
 def productor(cola, semaforo_P, semaforo_C):
-    for i in range (100):
+    for i in range (100):  # Producimos X productos
         producto_generado = f"producto {i}"
-        semaforo_P.acquire()
-        cola.put(producto_generado)
+        semaforo_P.acquire() # Adquirimos el semaforo del productor
+        cola.put(producto_generado) # Agregamos el producto a la cola
         print(f"-P: producto {i} agregado")
-        semaforo_C.release()
-        time.sleep(1)
+        semaforo_C.release() # Liberamos el semaforo del consumidor
+        time.sleep(2)
 
         
         
@@ -16,11 +16,11 @@ def productor(cola, semaforo_P, semaforo_C):
         
 def consumidor(cola, semaforo_P, semaforo_C):
     while True:
-        semaforo_C.acquire()
-        producto_generado = cola.get()
-        print(f"-C: {producto_generado} consumido")
-        semaforo_P.release()
-        time.sleep(2)
+        semaforo_C.acquire() # Adquirimos el semaforo del consumidor
+        producto_generado = cola.get() # Obtenemos el producto de la cola
+        print(f"-C: {producto_generado} consumido") 
+        semaforo_P.release() # Liberamos el semaforo del productor
+        time.sleep(3)
         
     
 
