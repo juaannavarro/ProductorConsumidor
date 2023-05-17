163bae568aef35aff79cde48b229a7669e2ed8ba
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2023-05-17 11:24:59 +0200

    aplicacion semaforos

--- Productor_Consumidor.py
+++ Productor_Consumidor.py
@@ -1,19 +1,26 @@
-import time , random
+import time 
 import multiprocessing
 import queue
-def productor(cola):
-    for i in range (10):
+def productor(cola, semaforo_P, semaforo_C):
+    for i in range (100):
         producto_generado = f"producto {i}"
+        semaforo_P.acquire()
         cola.put(producto_generado)
         print(f"-P: producto {i} agregado")
+        semaforo_C.release()
         time.sleep(1)
+
         
         
-def consumidor(cola):
+        
+        
+def consumidor(cola, semaforo_P, semaforo_C):
     while True:
+        semaforo_C.acquire()
         producto_generado = cola.get()
         print(f"-C: {producto_generado} consumido")
-        time.sleep(3)
+        semaforo_P.release()
+        time.sleep(4)
         
     
 
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from Productor_Consumidor import *
 if __name__ == "__main__":
-    cola = multiprocessing.Queue(2)
-    Proceso_productor = multiprocessing.Process(target=productor, args=(cola,))
-    Proceso_consumidor = multiprocessing.Process(target=consumidor, args=(cola,))
+    cola = multiprocessing.Queue(5)
+    
+    semaforo_P = multiprocessing.Semaphore(5)
+    semaforo_C = multiprocessing.Semaphore(0)
+    
+    Proceso_productor = multiprocessing.Process(target=productor, args=(cola, semaforo_P, semaforo_C))
+    Proceso_consumidor = multiprocessing.Process(target=consumidor, args=(cola, semaforo_P, semaforo_C))
     
     Proceso_productor.start()
     Proceso_consumidor.start()
